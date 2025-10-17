588247722f3e072b69ccc9948e11354e6ddc6877
Chad Malone Murphy <Chad.Malone_Murphy@sita.aero>
2025-10-17 16:24:48 +0100

    clean slate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HousingListComponent } from './housing-list/housing-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HousingListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
