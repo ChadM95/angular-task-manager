9ea4d87543de03aca93cfe41d4509568c771b26f
Chad Malone Murphy <Chad.Malone_Murphy@sita.aero>
2025-10-20 11:27:50 +0100

    create header, body1 & body2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { Body1Component } from './body1/body1.component';
+import { Body2Component } from './body2/body2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Body1Component,
+    Body2Component
   ],
   imports: [
     BrowserModule
